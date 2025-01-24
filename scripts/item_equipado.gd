85ee531cb3e31923151bb3b5ee2f52ea5f285d1c
Mariano <97608505+Ferbinand@users.noreply.github.com>
2025-01-23 23:53:48 -0300

    imagen peor de prueba

--- scripts/item_equipado.gd
+++ scripts/item_equipado.gd
@@ -11,7 +11,7 @@ func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
 	
 	rotation_degrees = wrap(rotation_degrees, 0, 360)
-	if (rotation_degrees > 90 and rotation_degrees < 270):
+	if (rotation_degrees > 90 and rotation_degrees < 180):
 		scale.y = -1
 	else:
 		scale.y = 1
